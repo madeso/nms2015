5276565c528ed82fe194f54c449914cc63d15e60
Gustav <sir.gustav.the.coder@gmail.com>
2015-07-17 20:24:24 +0200

    hooked up logic to graphics

---
+++ Assets/Scripts/PlayerPosition.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPosition : MonoBehaviour {
+
+	private float position_ = 0;
+
+	int index_;
+
+	Masher masher;
+
+	public float Position {
+		get {
+			return this.position_;
+		}
+	}
+
+	public void Setup (Players players, int index)
+	{
+		this.players_ = players;
+		this.index_ = index;
+	}
+
+	void Start() {
+		this.masher = this.GetComponent<Masher>();
+		if( this.masher == null ) throw new UnityException("no masher component on object");
+	}
+
+	public void Update() {
+		position_ += this.masher.GetValue() * Time.deltaTime;
+		this.transform.position = this.players_.GetStartPosition(this.index_) + Vector3.right * position_;
+	}
+
+	private Players players_;
+}
---
+++ Assets/Scripts/Players.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+//using UnityEngine.Assertions;
+
+public class Players : MonoBehaviour {
+
+	public PlayerPosition[] PlayerList;
+	public GameObject[] StartPositions;
+
+	public Vector3 GetStartPosition (int index_)
+	{
+		return this.StartPositions[index_].transform.position;
+	}
+
+	void Start() {
+		//UnityEngine.Assert.AreEqual
+		AssertAreEqual(this.StartPositions.Length, this.PlayerList.Length);
+		int index = 0;
+		foreach(var pl in PlayerList) {
+			pl.Setup(this, index);
+			++index;
+		}
+	}
+
+	private static void AssertAreEqual (int i, int i2)
+	{
+		if( i != i2 ) throw new UnityException("Assert failure");
+	}
+}
