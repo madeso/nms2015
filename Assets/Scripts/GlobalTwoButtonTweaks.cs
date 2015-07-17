fcc1fff6f1a546ad5d864877c71a4219bd48ae5a
Gustav <sir.gustav.the.coder@gmail.com>
2015-07-17 21:09:24 +0200

    tweaks to buttonmashing

--- Assets/Scripts/GlobalTwoButtonTweaks.cs
+++ Assets/Scripts/GlobalTwoButtonTweaks.cs
@@ -2,17 +2,11 @@
 using System.Collections;
 
 public class GlobalTwoButtonTweaks : MonoBehaviour {
-
-	public float RankDelta (float d, float i)
-	{
-		var delta = Mathf.Abs(d);
-		var interval = Mathf.Abs(i);
-		var beatmatch = this.BeatMatch.Evaluate(interval);
-		var speedinterval = this.SpeedInterval.Evaluate(delta);
-		Debug.Log(string.Format("delta={0}, interval={1}, beatmatch={2}, imatch={3}", delta, interval, beatmatch, speedinterval));
-		return beatmatch * speedinterval;
-	}
-
+	
 	public AnimationCurve BeatMatch; /// how good you match the beat
 	public AnimationCurve SpeedInterval; /// how fast you move per beat
+
+	public float SpeedScale = 1; // how much you gain each press
+	public float DescreaseScale = 1; /// how fast the value decrease
+	public float MaxSpeed = 3;
 }
--- Assets/Scripts/TwoButtonMasher.cs
+++ Assets/Scripts/TwoButtonMasher.cs
@@ -7,9 +7,7 @@ public class TwoButtonMasher : MonoBehaviour, Masher {
 	public KeyCode RightKeyCode = KeyCode.S;
 
 
-	public float SpeedScale = 1; // how much you gean each press
-	public float DescreaseScale = 1; /// how fast the value decrease
-	public float MaxSpeed = 3;
+
 	public GlobalTwoButtonTweaks Tweak;
 
 	public float timer_;
@@ -25,21 +23,45 @@ public class TwoButtonMasher : MonoBehaviour, Masher {
 	float Masher.GetValue() {
 		return value_;
 	}
+
+	public float interval;
+
+	public float delta;
+
+	public float beatmach;
+
+	public float speedinterval;
+
+	public float RankDelta (float d, float i)
+	{
+		var delta = Mathf.Abs(d);
+		var interval = Mathf.Abs(i);
+		var beatmatch = this.Tweak.BeatMatch.Evaluate(interval);
+		var speedinterval = this.Tweak.SpeedInterval.Evaluate(delta);
+
+		this.interval = interval;
+		this.delta = delta;
+		this.beatmach = beatmatch;
+		this.speedinterval = speedinterval;
+
+		Debug.Log(string.Format("delta={0}, interval={1}, beatmatch={2}, imatch={3}", delta, interval, beatmatch, speedinterval));
+		return beatmatch * speedinterval;
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		this.timer_ += Time.deltaTime;
-		this.value_ -= Time.deltaTime * this.DescreaseScale;
+		this.value_ -= Time.deltaTime * this.Tweak.DescreaseScale;
 		if( value_ < 0 ) value_ = 0;
 		var down = left_ ? Input.GetKey(this.LeftKeyCode) : Input.GetKey(this.RightKeyCode);
 		if( down == false ) return;
 		var delta = Mathf.Abs(timer_)-Mathf.Abs(lastDelta_);
-		this.value_ += this.Tweak.RankDelta(timer_, delta) * SpeedScale;
-		if( this.value_ > this.MaxSpeed ) {
-			this.value_ = this.MaxSpeed;
+		this.value_ += this.RankDelta(timer_, delta) * this.Tweak.SpeedScale;
+		if( this.value_ > this.Tweak.MaxSpeed ) {
+			this.value_ = this.Tweak.MaxSpeed;
 			Debug.Log("Hit roof");
 		}
-		lastDelta_ = delta;
+		lastDelta_ = this.timer_;
 		left_ = !left_;
 		this.timer_ = 0;
 	}
