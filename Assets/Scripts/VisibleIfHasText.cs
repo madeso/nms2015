be883d458b80972e356850958ec3cfe7174f8b4f
Gustav <sir.gustav.the.coder@gmail.com>
2015-07-19 10:38:37 +0200

    added background to the dictator tweet

---
+++ Assets/Scripts/VisibleIfHasText.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class VisibleIfHasText : MonoBehaviour {
+
+	public Text TextToCheck;
+
+	private CanvasRenderer renderer_;
+
+	public void Start() {
+		if( this.TextToCheck == null ) {
+			TextToCheck = this.gameObject.GetComponentInParent<Text>();
+		}
+		this.renderer_ = this.GetComponent<CanvasRenderer>();
+	}
+
+	public void LateUpdate() {
+		var text = this.TextToCheck.text;
+		var has_text = text.Length > 0;
+		renderer_.SetAlpha(has_text ? 1.0f : 0.0f);
+	}
+}
